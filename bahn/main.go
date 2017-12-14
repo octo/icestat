e5a3b81dcd254c5db7cb567fca541b9e2d3b401c
Florian Forster <ff@octo.it>
2017-12-14 09:39:47 +0100

    Package bahn: Remove the unused Position code.
    The API has been removed from the on-board server.

--- bahn/main.go
+++ bahn/main.go
@@ -4,24 +4,3 @@ Package bahn implements methods for querying live trip information from within I
 See the LICENSE file for licensing details.
 */
 package bahn // import "github.com/octo/icestat/bahn"
-
-import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"io/ioutil"
-)
-
-func unmarshalJSONP(r io.ReadCloser, v interface{}) error {
-	defer r.Close()
-
-	b, err := ioutil.ReadAll(r)
-	if err != nil {
-		return err
-	}
-
-	b = bytes.TrimLeft(b, "(")
-	b = bytes.TrimRight(b, ");\r\n")
-
-	return json.Unmarshal(b, v)
-}
